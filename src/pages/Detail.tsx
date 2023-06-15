9bb4b5323b874afd095f23fdfc78921a4587924f
userJAMT <jmendez4tob@gmail.com>
2023-06-15 11:57:23 -0300

    dinamic titles of website

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,6 +1,7 @@
 import useSWR from 'swr'
 import { getItemInfo } from '../services/hacker-news'
 import { ListOfComments } from '../components/ListOfComments'
+import { useEffect } from 'react'
 
 export default function Detail (props: {
   params: {
@@ -9,7 +10,12 @@ export default function Detail (props: {
 }) {
   const { params: { id } } = props
   const { data, isLoading } = useSWR(`story/${id}`, () => getItemInfo(Number(id)))
-  const commentIds = data?.kids?.slice(0, 10) ?? []
+  const { title, kids }: { title: string, kids: number[] } = data
+  const commentIds = kids?.slice(0, 10) ?? []
+
+  useEffect(() => {
+    document.title = `Hacker News Clone - ${title}`
+  }, [title])
 
   return (
     <div>
